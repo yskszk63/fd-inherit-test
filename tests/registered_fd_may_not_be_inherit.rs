1d321b0a3f209c44fb85a0ba909f4b604af2a621
yskszk63 <yskszk63@gmail.com>
2021-07-01 18:23:51 +0900

    no CLOEXEC

--- tests/registered_fd_may_not_be_inherit.rs
+++ tests/registered_fd_may_not_be_inherit.rs
@@ -6,22 +6,6 @@ use std::process::Command;
 
 use tokio::io::unix::AsyncFd;
 
-fn set_cloexc(fd: RawFd) -> io::Result<()> {
-    let flags = unsafe {
-        libc::fcntl(fd, libc::F_GETFD)
-    };
-    if flags == -1 {
-        return Err(io::Error::last_os_error());
-    }
-    let r = unsafe {
-        libc::fcntl(fd, libc::F_SETFL, flags | libc::FD_CLOEXEC)
-    };
-    if r == -1 {
-        return Err(io::Error::last_os_error());
-    }
-    Ok(())
-}
-
 fn set_nonblocking(fd: RawFd) -> io::Result<()> {
     let r = unsafe {
         libc::fcntl(fd, libc::F_GETFL)
@@ -41,7 +25,7 @@ fn set_nonblocking(fd: RawFd) -> io::Result<()> {
 }
 
 
-fn new_closexec_nonblocking_pipe() -> io::Result<(c_int, c_int)> {
+fn new_nonblocking_pipe() -> io::Result<(c_int, c_int)> {
     let mut pair = [0; 2];
     let r = unsafe {
         libc::pipe(pair.as_mut_ptr()) // no CLOEXEC (pipe2 does not exist on mac)
@@ -51,7 +35,6 @@ fn new_closexec_nonblocking_pipe() -> io::Result<(c_int, c_int)> {
     }
 
     for fd in pair {
-        set_cloexc(fd)?;
         set_nonblocking(fd)?;
     }
 
@@ -91,8 +74,8 @@ fn dup2(fd: RawFd, newfd: RawFd) -> io::Result<()> {
 
 #[tokio::test]
 async fn test_registered_fd_mai_not_be_inherit() -> io::Result<()> {
-    // CLOEXEC, NONBLOCKINGなパイプを作成
-    let (r, w) = new_closexec_nonblocking_pipe()?;
+    // NONBLOCKINGなパイプを作成 CLOEXECは付与しない
+    let (r, w) = new_nonblocking_pipe()?;
 
     // tokioのI/O Driverに登録
     // Linuxはepoll, Macはkqueue
@@ -110,11 +93,8 @@ print(os.stat(3))
         // AsyncFd自体は破棄しない
         let rfd = *r.get_ref();
         command.pre_exec(move || {
-            // CLOEXECを外すためにdup
-            let t = dup(rfd)?;
             // pythonがstatする3番目のファイル記述子にセット
-            dup2(t, 3)?;
-            close(t)?;
+            dup2(rfd, 3)?;
             Ok(())
         });
         let result = command.spawn();
